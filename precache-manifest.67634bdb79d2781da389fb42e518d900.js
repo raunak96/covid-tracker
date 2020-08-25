bc27276605cc6b5f5e7c568639e7c13305a17448
Raunak Kedia <raunakkedia1@gmail.com>
2020-08-26 00:48:18 +0530

    Updates

---
+++ precache-manifest.67634bdb79d2781da389fb42e518d900.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e6d6dab8a4a2ce4b1ef8dfd673338f",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "ec0f923d88a03c2dcef9",
+    "url": "/covid-tracker/static/css/main.3b9cd311.chunk.css"
+  },
+  {
+    "revision": "37a8c7b37dedbee5f4ea",
+    "url": "/covid-tracker/static/js/2.2a7d1874.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/covid-tracker/static/js/2.2a7d1874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec0f923d88a03c2dcef9",
+    "url": "/covid-tracker/static/js/main.cc4cb90b.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/covid.d7265326.png"
+  },
+  {
+    "revision": "c38d59b013ef43746bc1033e3340cb56",
+    "url": "/covid-tracker/static/media/loader.c38d59b0.gif"
+  }
+]);
